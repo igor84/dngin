429f04e12d6250a611a2d7ec382a2a04d0c228d9
igor84 <stojkovic.igor@gmail.com>
2017-08-19 19:40:42 +0200

    Basic window creation and loop

--- source/winmain.d
+++ source/winmain.d
@@ -1,7 +1,11 @@
 module winmain;
 
+version (Windows):
+
 import core.runtime;
 import core.sys.windows.windows;
+
+bool GlobalRunning = true;
 
 extern (Windows)
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
@@ -21,7 +25,60 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
     return result;
 }
 
-int myWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    /* ... insert user code here ... */
+int myWinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmdLine, int cmdShow) {
+    WNDCLASS WindowClass = {
+        style: CS_HREDRAW | CS_VREDRAW,
+        lpfnWndProc: cast(WNDPROC)&Win32MainWindowCallback,
+        hInstance: instance,
+        hCursor: LoadCursor(null, IDC_ARROW),
+        lpszClassName: "DNginWindowClass",
+    };
+
+    if (!RegisterClass(&WindowClass)) return 1;
+
+    enum WindowWidth = 1366;
+    enum WindowHeight = 768;
+
+    HWND Window = CreateWindowEx(
+                                 0, //WS_EX_TOPMOST|WS_EX_LAYERED,       // extended window style
+                                 WindowClass.lpszClassName,          // previously registered class to create
+                                 "DNgin",                            // window name or title
+                                 WS_OVERLAPPEDWINDOW|WS_VISIBLE,     // window style
+                                 CW_USEDEFAULT,                      // X
+                                 CW_USEDEFAULT,                      // Y
+                                 WindowWidth,
+                                 WindowHeight,
+                                 null,                               // Parent Window
+                                 null,                               // Menu
+                                 instance,                           // module instance handle
+                                 null                                // lpParam for optional additional data to store with the win
+                                );
+    if (!Window) return 1;
+
+    while (GlobalRunning) {
+        MSG msg;
+        while (PeekMessage(&msg, null, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
     return 0;
+}
+
+extern(Windows)
+LRESULT Win32MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam) {
+    LRESULT Result;
+
+    switch(Message) {
+        case WM_QUIT, WM_CLOSE:
+            GlobalRunning = false;
+            break;
+
+        default:
+            Result = DefWindowProc(Window, Message, WParam, LParam);
+            break;
+    }
+
+    return Result;
 }
